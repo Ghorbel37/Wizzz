d1a28e6602849ba22ea188169e5c992545218bd9
Mahdi <mghorbel1@yahoo.fr>
2024-01-26 17:07:16 +0100

    Initial commit

---
+++ Wizzz_backup.ps1
@@ -0,0 +1,168 @@
+﻿##########################
+#####Global variables#####
+##########################
+
+# Set the running location
+$RunLocation = $PSScriptRoot
+$BackupRoot = "C:\Backup"
+
+# Set the excluded folders ( Folders that have too many files )
+#Separate with pipe "|"
+$Excluded= """$env:WINDIR""|""$env:LOCALAPPDATA\Yarn\"""
+
+# Locations that should have backup folder
+# Separate with comma ","
+$destinationDrives = "D:", "E:", "F:" 
+
+##########################
+###Global variables end###
+##########################
+
+
+# Parse command-line arguments
+foreach ($arg in $args) {
+    switch ($arg) {
+        "-silent" { $silentMode = $true }
+    }
+}
+
+# Function to display script information
+function DisplayScriptInformation() {
+    Write-Host "`n`nWelcome to the Backup Script" -ForegroundColor Cyan
+    Write-Host "This script exports filesystem structure to csv using WizTree," -ForegroundColor Cyan
+    Write-Host "compresses the exported files, and copies the backup to multiple locations." -ForegroundColor Cyan
+    if($silentMode = $true){
+        Write-Host "Script is running in silent mode" -ForegroundColor Yellow
+     }
+    Write-Host "`n`n"
+}
+
+# Display script information
+DisplayScriptInformation
+
+
+# Check if the script is running with elevated privileges and restart it if not
+$isAdmin = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+if (-not $isAdmin) {
+    Write-Output "This script needs to run as an administrator"
+    Write-Output "It will restart itself with administrator privileges."
+    Write-Output "Press any key to continue..."
+    $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+    Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+    exit
+}
+
+# Check if the WizTree executable exists
+$WizTreeExePath = Join-Path $RunLocation "WizTree.exe"
+if (-not (Test-Path $WizTreeExePath -PathType Leaf)) {
+    Write-Output "WizTree.exe not found. Make sure it is in the script directory."
+    Write-Output "Press any key to exit..."
+    $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+    exit
+}
+
+# Get the current date and time for creating a unique folder
+$BackupFolderName = Get-Date -Format "yyyyMMdd_HHmmss"
+$BackupLocation = Join-Path $BackupRoot $BackupFolderName
+
+
+# Display all drives in a table with additional information
+Write-Host "Available drives:`n"
+$drives = Get-WmiObject Win32_LogicalDisk | Select-Object DeviceID, VolumeName, Size, FreeSpace, FileSystem
+$drives | Format-Table -AutoSize
+
+
+# Ask for confirmation
+if (-not $silentMode) {
+    $confirmation = Read-Host "Do you want to proceed with the backup? (Y/n)"
+    if (-not $confirmation) {
+        $confirmation = 'Y'
+    }
+    if ($confirmation -ne 'Y') {
+        Write-Host "Backup aborted."
+        Write-Output "Press any key to exit..."
+        $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+        exit
+    }
+}
+
+# Create a new folder for each backup
+Write-Output "`nCreating temporary backup folder"
+$null=New-Item -ItemType Directory -Path $BackupLocation -Force
+Write-Output "Done.`n"
+
+
+
+# Get all logical drives
+$drives = Get-WmiObject Win32_LogicalDisk | Select-Object DeviceID
+
+# Display the list
+foreach ($drive in $drives) {
+    # Get DriveName And CsvName
+    $DriveName = $drive.DeviceID
+    $CsvName=( $drive.DeviceID[0] + "_%d_%t.csv")
+        # System drive
+        if($drive.DeviceID -eq $env:SystemDrive){
+            Write-Output "Saving backup for $DriveName"
+            Start-Process -FilePath "$RunLocation\WizTree.exe" -ArgumentList "$DriveName /export=""$BackupLocation\$CsvName"" /admin=1 /filterexclude=$Excluded" -Wait
+            
+        }
+        # Other drives
+        else {
+            Write-Output "Saving backup for $DriveName"
+            Start-Process -FilePath "$RunLocation\WizTree.exe" -ArgumentList "$DriveName /export=""$BackupLocation\$CsvName"" /admin=1 /filterexclude=$Excluded" -Wait
+        }
+}
+Write-Output "Backup complete`n"
+
+# Compressing all files
+Write-Output "Compressing all exported CSV files into a single archive"
+$ArchiveName = "Backup_$BackupFolderName.zip"
+$ArchivePath = Join-Path $BackupRoot $ArchiveName
+Compress-Archive -Path "$BackupLocation\*.csv" -DestinationPath $ArchivePath -CompressionLevel "Optimal" -Force
+Write-Output "Done.`n"
+
+# Copy the backup file to different drives
+foreach ($destinationDrive in $destinationDrives) {
+    $destinationPath = Join-Path $destinationDrive "Backup"
+    $destinationFilePath = Join-Path $destinationPath $ArchiveName
+
+    # Create Backup folder if it doesn't exist
+    if (-not (Test-Path $destinationPath -PathType Container)) {
+        New-Item -ItemType Directory -Path $destinationPath -Force
+    }
+
+    Write-Output "Copying backup to $destinationDrive"
+    Copy-Item -Path $ArchivePath -Destination $destinationFilePath -Force
+}
+Write-Output "Copied backup to all specified drives`n"
+
+# Cleanup temp CSV files
+Write-Output "Cleaning temp CSV folder"
+
+# Check if silent flag is set
+if (-not $silentMode) {
+    $confirmationDelete = Read-Host "Do you want to clean temporary backup files? (Y/n)"
+    if (-not $confirmationDelete) {
+        $confirmationDelete = 'Y'
+    }
+} else {
+    $confirmationDelete = 'Y'
+}
+if ($confirmationDelete -eq 'Y') {
+    Remove-Item -Path $BackupLocation -Recurse -Force
+    Write-Output "Cleaning temp CSV folder completed.`n"
+} else {
+    Write-Output "Cleanup cancelled.`n"
+}
+
+
+
+# Prompt to press any key to exit
+Write-Host "Backup completed successfully."
+Write-Host "Saved file to: *:\Backup\$ArchiveName `n"
+if(-not $silentMode){Write-Output "Press any key to exit..."
+$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+}
+
+# End of script
---
+++ Wizzz_create_task.ps1
@@ -0,0 +1,17 @@
+﻿# Define the script path
+$scriptPath = "$PSScriptRoot\Wizzz_Backup.ps1"
+$taskName = "Wiztree Filesystem Backup"
+$taskDescription = "Takes a copy of filesystem structure and saves it in zip files in *:\Backup\"
+$user = [System.Security.Principal.WindowsIdentity]::GetCurrent().User.Value
+
+# Define the action to be performed by the task (run PowerShell)
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoProfile -ExecutionPolicy Bypass -File `"$scriptPath`" -silent"
+
+# Define the trigger (e.g., run daily at 2:00 AM)
+$currentDateTime = Get-Date
+
+# Define the trigger (weekly starting from January 1, 1999, 00:00)
+$trigger = New-ScheduledTaskTrigger -Weekly -At "23:12" -DaysOfWeek Sunday
+
+# Register the scheduled task with the action and trigger
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName $taskName -Description $taskDescription -RunLevel Highest -User $user
